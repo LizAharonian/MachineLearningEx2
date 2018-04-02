c698d9cdac08a533f4cad79e059b6448a9e83d3d
LizAharonian <lizaharonian@gmail.com>
2018-04-02 19:50:05 +0300

    Initial commit

---
+++ ex2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys
+
+def main():
+    """""
+    main function.
+    runs the program.
+    argv[1] - command line argument.
+    """""
+    input_text_file = sys.argv[1]
+
+
+if __name__ == '__main__':
+    main()
